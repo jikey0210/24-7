18d81caddfb9317e8d11a650e0f93edd153c0f3f
jikey0210 <88705593+jikey0210@users.noreply.github.com>
2024-02-24 00:10:15 +0700

    Update index.js

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ const { joinVoiceChannel } = require('@discordjs/voice');
 client.on('ready', () => {
   client.user.setActivity("Youtube", { type: "STREAMING", url: "https://www.youtube.com/watch?v=Zming5TY_pQ" })
   setInterval(async () => {
-    client.channels.fetch("1210630714589184021")
+    client.channels.fetch("1210634511168045116")
       .then((channel) => {
         const VoiceConnection = joinVoiceChannel({
           channelId: channel.id,
